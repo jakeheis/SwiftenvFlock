a7c7405b97c6b3e58bf1240adbebc337c614ad9b
Jake Heiser <jakeheiser1@gmail.com>
2017-01-03 15:16:07 -0600

    Fixing dependency

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "SwiftenvFlock",
     dependencies: [
-        .Package(url: "/Users/jakeheiser/Desktop/Projects/Swift/Flock", majorVersion: 0, minor: 1)
+        .Package(url: "https://github.com/jakeheis/Flock", majorVersion: 0, minor: 1)
     ]
 )
